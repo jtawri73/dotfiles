dca850c71d6dae76d2696a900095e87399b0e11d
jtawri73 <jtawri73@gmail.com>
2025-06-16 14:27:29 +0530

    dotfiles: user: firefox: set toolbar layout

--- user/programs/firefox.nix
+++ user/programs/firefox.nix
@@ -10,6 +10,22 @@
       isDefault = true;
       settings = {
         "extensions.autoDisableScopes" = 0;
+
+        "browser.uiCustomization.state" = {
+          placements = {
+            widget-overflow-fixed-list = [];
+            unified-extensions-area = [];
+            nav-bar = ["back-button" "forward-button" "stop-reload-button" "urlbar-container" "downloads-button" "unified-extensions-button"];
+            toolbar-menubar = ["menubar-items"];
+            TabsToolbar = ["tabbrowser-tabs" "new-tab-button"];
+            vertical-tabs = [];
+            PersonalToolbar = ["personal-bookmarks"];
+          };
+          seen = ["save-to-pocket-button" "developer-button" "ublock0_raymondhill_net-browser-action" "sponsorblocker_ajay_app-browser-action"];
+          dirtyAreaCache = ["nav-bar" "vertical-tabs" "PersonalToolbar" "toolbar-menubar" "TabsToolbar" "unified-extensions-area"];
+          currentVersion = 22;
+          newElementCount = 2;
+        };
       };
       extensions.packages = with inputs.firefox-addons.packages.${pkgs.system}; [
         sponsorblock
