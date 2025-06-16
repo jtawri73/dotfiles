e8241144db5818b04d0987e355d8d78a88ac33da
jtawri73 <jtawri73@gmail.com>
2025-06-16 14:17:29 +0530

    dotfiles: user: firefox: install sponsorblock and ublock-origin addons

--- user/programs/firefox.nix
+++ user/programs/firefox.nix
@@ -1,10 +1,20 @@
 {
   config,
+  inputs,
   pkgs,
   ...
 }: {
   programs.firefox = {
     enable = true;
-    profiles.marika.isDefault = true;
+    profiles.marika = {
+      isDefault = true;
+      settings = {
+        "extensions.autoDisableScopes" = 0;
+      };
+      extensions.packages = with inputs.firefox-addons.packages.${pkgs.system}; [
+        sponsorblock
+        ublock-origin
+      ];
+    };
   };
 }
