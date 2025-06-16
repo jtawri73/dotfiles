0642c5502be574bcac8fa9b79aeafb1436e7db24
jtawri73 <jtawri73@gmail.com>
2025-06-16 11:03:11 +0530

    dotfiles: user: fish: enable vi key bindings

--- user/programs/fish.nix
+++ user/programs/fish.nix
@@ -8,6 +8,12 @@
     interactiveShellInit = ''
       # Disable greeting
       set fish_greeting
+
+      # Enable vi key bindings
+      fish_vi_key_bindings
+
+      # Set insert mode cursor to a block
+      set fish_cursor_insert block
     '';
   };
 }
